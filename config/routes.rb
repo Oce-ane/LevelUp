166b31688e01de1fff8ea543b3ba0efb7fc2755c
Oceane BOUNAN <bounan.oceane@orange.fr>
2024-06-07 23:44:19 +0400

    navbar and user page added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -12,4 +13,5 @@ Rails.application.routes.draw do
   resources :skills, only: %i[index show] do
     resources :levels, only: %i[index update]
   end
+  resources :users, only: %i[show]
 end
