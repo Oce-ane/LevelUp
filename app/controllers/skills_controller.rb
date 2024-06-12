4b1ef4390dbd31f051079f48b2ce18491c14a2a7
Oceane BOUNAN <bounan.oceane@orange.fr>
2024-06-12 04:50:30 +0400

    skill selection handled

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -2,8 +2,8 @@ class SkillsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @selected_skills = current_user.skills
     @all_skills = Skill.all
+    @selected_skills = current_user.skills
     @selected_skill_ids = @selected_skills.pluck(:id)
   end
 
