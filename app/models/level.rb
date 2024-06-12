e32b985a08a848f350f81c8e50e8298b18ac8562
Oceane BOUNAN <bounan.oceane@orange.fr>
2024-06-12 04:51:31 +0400

    level seletion handled

--- app/models/level.rb
+++ app/models/level.rb
@@ -3,4 +3,12 @@ class Level < ApplicationRecord
 
   validates :name, presence: true
   validates :exercise, presence: true, uniqueness: true
+
+  after_save :update_skill_progression, if: :saved_change_to_completed?
+
+  private
+
+  def update_skill_progression
+    skill.update_progression
+  end
 end
